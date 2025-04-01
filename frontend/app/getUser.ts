fe2927556e685f408f7884032cb063bf67fffa94
aleksander <147505187+baskaaleksander@users.noreply.github.com>
2025-04-01 17:13:51 +0200

    frontend project remake

--- frontend/app/getUser.ts
+++
@@ -1,8 +0,0 @@
-import { parse } from "cookie";
-
-export async function getServerSideProps({ req }: { req: { headers: { cookie?: string } } }) {
-  const cookies = parse(req.headers.cookie || "");
-  const token = cookies.jwt || null;
-
-  return { props: { token } };
-}
